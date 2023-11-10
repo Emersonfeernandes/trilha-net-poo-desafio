3b2fd1cb6ed1ce2ab0ff21c1ffe535b54a2ab42f
Emersonfeernandes <emersonpoeta3000@gmail.com>
2023-11-09 22:21:52 -0300

    Desafio POO da dio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999", "ultimo modelo", "111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+
+Iphone iphone = new Iphone("77777", "Iphone 10", "333", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
